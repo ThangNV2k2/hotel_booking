6fd9e3feaa97fc31b521bedab26ff961ef69a3ef
ThangNV2k2 <thang08052002@gmail.com>
2024-09-15 21:50:31 +0700

    initial vite react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,35 @@
-<<<<<<< HEAD
-import "./App.scss";
-import { Button } from "@/components/ui/button";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  return <Button>Hello</Button>;
-}
+  const [count, setCount] = useState(0)
 
-export default App;
-=======
-import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
-import { ReloadIcon } from "@radix-ui/react-icons"
-
-function App() {
   return (
     <>
-      <p className="text-red-300">hello</p>
-      <Button>Click me</Button>
-      <Checkbox />
-      <Button disabled>
-        <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-        Please wait
-      </Button>
-      <div className="bg-success h-4 w-full"></div>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
 
 export default App
->>>>>>> 276a2f89898cecbffff6c146f86995e4d90eb624
